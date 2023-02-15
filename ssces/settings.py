756878aa3ea39bb42adab5cb64d9ee27dbc0843b
mohammad-esmaeely <esmaeely1377@gmail.com>
2023-02-15 19:47:16 +0330

    core(setting): add static settings

--- ssces/settings.py
+++ ssces/settings.py
@@ -139,7 +139,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/upload')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATIC_ROOT = 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
