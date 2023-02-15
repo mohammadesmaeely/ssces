ccbcd9d7d5b451791ff124fdfe43a58fc8e706e6
mohammad-esmaeely <esmaeely1377@gmail.com>
2023-02-15 19:26:29 +0330

    core(setting): add static settings

--- ssces/settings.py
+++ ssces/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 import environ
 # Initialise environment variables
@@ -138,6 +138,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/upload')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
