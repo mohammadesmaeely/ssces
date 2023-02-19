3217aa72538318ba1d03b8335079eec66f182b09
mohammad-esmaeely <esmaeely1377@gmail.com>
2023-02-19 20:00:39 +0330

    ssces(urls): add static url to url patterns

--- ssces/urls.py
+++ ssces/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('events', include('events.urls')),
 ]
+urlpatterns += staticfiles_urlpatterns()
