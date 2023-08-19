4951d43efe5f87d452fd31b079d372a8fe7a89f4
mohammad-esmaeely <esmaeely1377@gmail.com>
2023-08-19 17:45:07 +0330

    feat(ssces): set media url

--- ssces/settings.py
+++ ssces/settings.py
@@ -142,6 +142,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/upload')
+MEDIA_URL = '/media/'
 STATIC_ROOT = 'static'
 
 # Default primary key field type
--- ssces/urls.py
+++ ssces/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
@@ -36,3 +38,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
