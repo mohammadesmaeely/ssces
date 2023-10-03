c30c4d64a9c65a46dfa3255759915b16f0745d20
mohammad-esmaeely <esmaeely1377@gmail.com>
2023-10-04 00:03:01 +0330

    feat(events): add registered count to event serializer

--- events/serializers/event.py
+++ events/serializers/event.py
@@ -5,6 +5,8 @@ from events.models import Event
 
 class EventSerializer(serializers.ModelSerializer):
     category_name = serializers.CharField(source='category.name', read_only=True)
+    authenticated_user_registered_count = serializers.SerializerMethodField()
+    anonymous_user_registered_count = serializers.SerializerMethodField()
     class Meta:
         model = Event
         fields = [
@@ -19,5 +21,13 @@ class EventSerializer(serializers.ModelSerializer):
             'authenticated_user_price',
             'anonymous_user_capacity',
             'anonymous_user_price',
-
+            'authenticated_user_registered_count',
+            'anonymous_user_registered_count',
         ]
+
+    def get_authenticated_user_registered_count(self, instance):
+        return instance.registered_events.filter(user__isnull=False, is_paid=True).count()
+
+    def get_anonymous_user_registered_count(self, instance):
+        return instance.registered_events.filter(user__isnull=True, is_paid=True).count()
+
